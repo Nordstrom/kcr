71c34da4af9cff33c706b53cf5f4246f35a5aa5b
Garza, Jose Angel Q <jose.garza@nordstrom.com>
2019-05-23 10:53:27 -0700

    Add aws-s3 dependency

--- src/main/kotlin/module-info.java
+++ src/main/kotlin/module-info.java
@@ -1,20 +1,28 @@
 module com.nordstrom.kafka.kcr {
 
+    //command-line args parser
     requires clikt;
 
     requires jdk.unsupported;
 
+    //kafka
     requires kafka.clients;
 
     requires kotlin.stdlib;
 
+    //kotlin coroutines and serialization
     requires kotlinx.coroutines.core;
     requires kotlinx.serialization.runtime;
 
+    //metrics
     requires micrometer.core;
     requires micrometer.registry.jmx;
     requires micrometer.registry.statsd;
 
+    //logging
     requires slf4j.api;
+
+    //aws
+    requires software.amazon.awssdk.services.s3;
 
 }
